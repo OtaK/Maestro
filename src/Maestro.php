9c179a70ff83509c0cbd661724ead1d381af31c4
Mathieu Amiot <m.amiot@otak-arts.com>
2014-06-09 11:24:29 +0200

    require stuff

--- src/Maestro.php
+++ src/Maestro.php
@@ -2,7 +2,8 @@
 
     namespace Maestro;
 
-    require_once __DIR__.'/vendor/autoload.php';
+    if (file_exists(__DIR__.'/../vendor/autoload.php'))
+        require_once __DIR__.'/../vendor/autoload.php';
 
     use Maestro\HTTP\Request;
     use Maestro\Renderer\Renderer;
